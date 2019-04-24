5582cfc36021611c29b515f1d275c7d85cf735e3
bbcawodu <awodubradley@gmail.com>
2019-04-24 14:10:35 -0500

    - Added Get Vowel Count Kata

---
+++ src/Katas/Models/VowelCountGetter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Katas.Models
+{
+    public class VowelCountGetter
+    {
+        private static readonly string vowels = "aeiou";
+
+        public static int GetVowelCount(string input)
+        {
+            return GetSumOfIndividualVowelCount(input);
+        }
+
+        // the count of vowels in an input string is either:
+        // the sum of the count of each individual vowel that is in the input
+        public static int GetSumOfIndividualVowelCount(string input)
+        {
+            int totalVowelCount = 0;
+
+            foreach (var vowel in vowels)
+            {
+                var vowelCount = input.Count(letter => letter == vowel);
+                totalVowelCount += vowelCount;
+            }
+
+            return totalVowelCount;
+        }
+
+        // the number of letters in the string that are a vowel
+        public static int GetNumberOfLettersThatAreVowel(string input)
+        {
+            return input.Count(letter => vowels.Contains(letter));
+        }
+    }
+}
---
+++ tests/KataTests/KataUnitTests/GetVowelCountShould.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Katas.Models;
+using NUnit.Framework;
+
+namespace KataTests.KataUnitTests
+{
+    [TestFixture]
+    class GetVowelCountShould
+    {
+        [Test]
+        [TestCase("abracadabra", ExpectedResult = 5)]
+        [TestCase("qoiuweyhuifbd", ExpectedResult = 6)]
+        [TestCase("ooooooo", ExpectedResult = 7)]
+        public int TestGetVowelCount(string input)
+        {
+            return VowelCountGetter.GetVowelCount(input);
+        }
+
+        [Test]
+        [TestCase("abracadabra", ExpectedResult = 5)]
+        [TestCase("qoiuweyhuifbd", ExpectedResult = 6)]
+        [TestCase("ooooooo", ExpectedResult = 7)]
+        public int TestGetSumOfIndividualVowelCount(string input)
+        {
+            return VowelCountGetter.GetSumOfIndividualVowelCount(input);
+        }
+
+        [Test]
+        [TestCase("abracadabra", ExpectedResult = 5)]
+        [TestCase("qoiuweyhuifbd", ExpectedResult = 6)]
+        [TestCase("ooooooo", ExpectedResult = 7)]
+        public int TestGetNumberOfLettersThatAreVowel(string input)
+        {
+            return VowelCountGetter.GetNumberOfLettersThatAreVowel(input);
+        }
+    }
+}
