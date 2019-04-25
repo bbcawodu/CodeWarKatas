e2800f829aae88caf22d1b68c8aa239bdc584792
bbcawodu <awodubradley@gmail.com>
2019-04-25 09:41:25 -0500

    - Refactoring

--- src/Katas/Models/VowelCountGetter.cs
+++ src/Katas/Models/VowelCountGetter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace Katas.Models
@@ -11,11 +12,17 @@ namespace Katas.Models
 
         public static int GetVowelCount(string input)
         {
+            bool vowelsInWordDefinitionsAreEqual =
+                GetSumOfIndividualVowelCount(input) ==
+                GetNumberOfLettersThatAreVowels(input);
+
+            if (!vowelsInWordDefinitionsAreEqual)
+            {
+                throw new Exception("Panic! Math is wrong.");
+            }
             return GetSumOfIndividualVowelCount(input);
         }
 
-        // the count of vowels in an input string is either:
-        // the sum of the count of each individual vowel that is in the input
         public static int GetSumOfIndividualVowelCount(string input)
         {
             int totalVowelCount = 0;
@@ -29,8 +36,7 @@ namespace Katas.Models
             return totalVowelCount;
         }
 
-        // the number of letters in the string that are a vowel
-        public static int GetNumberOfLettersThatAreVowel(string input)
+        public static int GetNumberOfLettersThatAreVowels(string input)
         {
             return input.Count(letter => vowels.Contains(letter));
         }
