8cabbef714d550cb44afee447c1100fbab0fb7d7
bbcawodu <awodubradley@gmail.com>
2019-04-26 13:51:38 -0500

    Refactoring

--- src/Katas/Models/MissingLetterFinder.cs
+++ src/Katas/Models/MissingLetterFinder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Katas.Models
 {
@@ -18,31 +19,43 @@ namespace Katas.Models
      */
     public class MissingLetterFinder
     {
-        public static char[] alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
-        public static char[] alphabetLower = Array.ConvertAll(alphabetUpper, letter => Char.ToLower(letter));
+        private static readonly char[] alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+        private static char[] alphabetToUse;
+        private static int alphabetStartIndex;
+        private static char missingLetter = ' ';
 
         public static char FindMissingLetter(char[] input)
         {
-            var alphabetToUse = GetAlphabetToUse(input);
-            var alphabetStartIndex = Array.IndexOf(alphabetToUse, input[0]);
+            GetAlphabetToUse(input);
+            GetAlphabetStartIndexFrom(input);
+            SearchForMissingLetter(input);
 
+            return missingLetter;
+        }
+
+        private static void GetAlphabetToUse(char[] input)
+        {
+            var inputContainsUpperCase = Array.Exists(alphabetUpper, element => element == input[0]);
+            alphabetToUse = inputContainsUpperCase ?
+                alphabetUpper :
+                Array.ConvertAll(alphabetUpper, letter => Char.ToLower(letter));
+        }
+
+        private static void GetAlphabetStartIndexFrom(char[] input)
+        {
+            alphabetStartIndex = Array.IndexOf(alphabetToUse, input[0]);
+        }
+
+        private static void SearchForMissingLetter(char[] input)
+        {
             for (var i = 0; i < input.Length; i += 1)
             {
                 var alphabetIndex = i + alphabetStartIndex;
                 if (input[i] != alphabetToUse[alphabetIndex])
                 {
-                    return alphabetToUse[alphabetIndex];
+                    missingLetter = alphabetToUse[alphabetIndex];
                 }
             }
-
-            return ' ';
-        }
-
-        private static char[] GetAlphabetToUse(char[] input)
-        {
-            var inputContainsUpperCase = Array.Exists(alphabetUpper, element => element == input[0]);
-            var alphabetToUse = inputContainsUpperCase ? alphabetUpper : alphabetLower;
-            return alphabetToUse;
         }
     }
 }
