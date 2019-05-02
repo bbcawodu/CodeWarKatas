6fb5617459cb2b782071d998085eb83fb7d604d5
bbcawodu <awodubradley@gmail.com>
2019-05-02 09:56:50 -0500

    first attempt at WordsMoreThan5LettersSpinner kata

---
+++ src/Katas/Models/WordsMoreThan5LettersSpinner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Katas.Models
+{
+    public class WordsMoreThan5LettersSpinner
+    {
+        private static List<string> _separatedWords;
+
+        public static string Spin(string words)
+        {
+            SeparateWords(words);
+            var spunSeparatedWords = SpinSeparatedWords();
+            return string.Join(" ", spunSeparatedWords);
+        }
+
+        private static void SeparateWords(string words)
+        {
+            _separatedWords = words.Split(
+                new string[] {" "},
+                StringSplitOptions.RemoveEmptyEntries
+            ).ToList();
+        }
+
+        private static List<string> SpinSeparatedWords()
+        {
+            var spunWords = _separatedWords.Select( word =>
+                (word.Length >= 5) ? string.Join("", word.Reverse()) : word
+            ).ToList();
+
+            return spunWords;
+        }
+    }
+}
---
+++ tests/KataTests/KataUnitTests/WordsMoreThan5LettersSpinnerShould.cs
@@ -0,0 +1,45 @@
+﻿using Katas.Models;
+using NUnit.Framework;
+
+namespace KataTests.KataUnitTests
+{
+    [TestFixture]
+    class WordsMoreThan5LettersSpinnerShould
+    {
+        [Test]
+        public static void Test1()
+        {
+            Assert.AreEqual("emocleW", WordsMoreThan5LettersSpinner.Spin("Welcome"));
+        }
+
+        [Test]
+        public static void Test2()
+        {
+            Assert.AreEqual("Hey wollef sroirraw", WordsMoreThan5LettersSpinner.Spin("Hey fellow warriors"));
+        }
+
+        [Test]
+        public static void Test3()
+        {
+            Assert.AreEqual("This is a test", WordsMoreThan5LettersSpinner.Spin("This is a test"));
+        }
+
+        [Test]
+        public static void Test4()
+        {
+            Assert.AreEqual("This is rehtona test", WordsMoreThan5LettersSpinner.Spin("This is another test"));
+        }
+
+        [Test]
+        public static void Test5()
+        {
+            Assert.AreEqual("You are tsomla to the last test", WordsMoreThan5LettersSpinner.Spin("You are almost to the last test"));
+        }
+
+        [Test]
+        public static void Test6()
+        {
+            Assert.AreEqual("Just gniddik ereht is llits one more", WordsMoreThan5LettersSpinner.Spin("Just kidding there is still one more"));
+        }
+    }
+}
